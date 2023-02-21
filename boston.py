b23e03b93206c3e43318c2d7f5c0339b59042877
John Williams <jrw+3@mit.edu>
2023-02-21 18:55:31 +0000

    Mallika

--- boston.py
+++ boston.py
@@ -8,3 +8,15 @@ url = "https://pollysnips.s3.amazonaws.com/bostonEmployeeSalaries.json"
 uh = urllib.request.urlopen(url)
 data = uh.read().decode()
 print('Retrieved', len(data), 'characters')
+print(type(data))
+print(data[:2])
+# print the structure of the data
+
+# load the data into a dictionary
+info = json.loads(data)
+myData = info["data"]
+
+print(myData[0:5])
+print("Total salary:"+  myData[0][18])
+print("Total salary:"+  myData[1][18])
+
